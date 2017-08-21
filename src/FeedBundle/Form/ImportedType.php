db078ef2d8f0ffc0005ac67b4ea4a583d95bf625
Darius K <kilinskis@gmail.com>
2017-08-22 00:30:26 +0300

    form fix

--- src/FeedBundle/Form/ImportedType.php
+++ src/FeedBundle/Form/ImportedType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ImportedType extends AbstractType
